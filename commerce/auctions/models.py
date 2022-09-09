e001ff53ea9df20ed6e0c00d1ce60270dadc5934
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-09-10 00:55:21 +0100

    creating a create.html template

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,6 +9,9 @@ class User(AbstractUser):
 class Category(models.Model):
     categoryName = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.categoryName
+    
 class Listing(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=300)
@@ -17,3 +20,6 @@ class Listing(models.Model):
     isActive = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
+    
+    def __str__(self):
+        return self.title
