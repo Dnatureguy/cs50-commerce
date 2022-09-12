9bbd2a9a0845d633647d84076063c2b33e56ff20
Muhammed Oladapo <muhammedoladapo3@gmail.com>
2022-09-12 23:55:52 +0100

    deleted and added new listing models

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -5,16 +5,40 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User, Category, Listing
+from .models import User, Category, Listing, Comment, Bid
 
 def listing(request, id):
     listingData = Listing.objects.get(pk=id)
     isListingInWatchlist = request.user in listingData.watchlist.all()
+    allComments = Comment.objects.filter(listing=listingData)
     return render(request, "auctions/listing.html", {
         "listing": listingData,
-        "isListingInWatchlist": isListingInWatchlist
+        "isListingInWatchlist": isListingInWatchlist,
+        "allComments": allComments
+    })
+
+def watchlist(request):
+    currentUser = request.user
+    listings = currentUser.listingWatchlist.all()
+    return render(request, "auctions/watchlist.html", {
+        "listings": listings
     })
     
+def addComment(request, id):
+    currentUser = request.user
+    listingData = Listing.objects.get(pk=id)
+    message = request.POST['newComment']
+        
+    newComment = Comment(
+        author=currentUser,
+        listing=listingData,
+        message=message
+    ) 
+     
+    newComment.save()
+    
+    return HttpResponseRedirect(reverse("listing", args=(id, )))
+
 def removeWatchlist(request, id):
     listingData = Listing.objects.get(pk=id)
     currentUser = request.user
@@ -65,12 +89,17 @@ def createListing(request):
         
         #Get all content about the particular category
         categoryData = Category.objects.get(categoryName=category)
+        
+        #Create a bid object
+        bid = Bid(bid=float(price), user=currentUser)
+        bid.save()
+        
         # Create a new Listing object
         newListing = Listing(
             title=title,
             description=description,
             imageURL=imageurl,
-            price=float(price),
+            price=bid,
             category=categoryData,
             owner=currentUser
         )
@@ -133,3 +162,5 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
